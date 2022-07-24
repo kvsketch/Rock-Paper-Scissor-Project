067b0e7fcac6652ed380075bbc433e8860b5fd6c
kvsketch <kellonv@gmail.com>
2022-07-24 14:10:22 -0600

    add HTML & Javascript files, add HTML skeleton+<script>.  Write code plan.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+//Create Computer Logic using getComputerChoice Function
+//getComputerChoice will return Rock, Paper, or Scissors randomly
+
+//Create a function playRound that plays a round of the game, using two parameters
+//playerSelection (use and input) and computerSelection (use function)
+//make player input case insensetive, can type the input anyway they'd like
+//use if statement to determine the winner
+//return string that delcares "You Lose!" + "Paper beats Rock", etc...
+
+//create function game() call playRound inside of this one use loop to 
+//play through 5 rounds of the game for (let i = 0; i < 5; i++) {
