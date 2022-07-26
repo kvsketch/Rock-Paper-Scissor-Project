a857dc1e82affddb7f959f54c09e56a36034463c
kvsketch <kellonv@gmail.com>
2022-07-25 21:41:41 -0600

    create playRound function making game logic (playerSelection() does not work in playRound a bug)

--- javascript.js
+++ javascript.js
@@ -11,16 +11,41 @@
 //play through 5 rounds of the game for (let i = 0; i < 5; i++) {
 
 
-const rockPaperScissors = ['Rock','Paper','Scissors'];
+const rockPaperScissors = ['rock','paper','scissors'];
 
 function getComputerChoice() {
     return rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
 }
 
-//take html input and print answer and return value of the player input
+//take html input and print answer and return value of the player input in lowercase
 const playerSelection = () => {
     let inputValue = document.getElementById("playerAnswer").value;
     document.getElementById("valueInput").innerHTML = inputValue;
     return inputValue.toLowerCase();
 }
 
+let computer = getComputerChoice();
+let player = prompt('Rock,Paper Scissors? Choose!');
+
+//playRound function sorts win scenarios, if they are not met, it will delcare a tie or L
+
+function playRound(player,computer) {
+    if ((player == 'rock' && computer == 'scissors') ||
+        (player == 'paper' && computer == 'rock') ||
+        (player == 'scissors' && computer == 'paper')) {
+            return 'You win!';
+        }
+    else if (player == computer){
+        return 'Tie!';
+    }
+    else {
+        return 'You lose!'
+        }
+
+}
+  //add value every round
+  //keep track of score and report winner at end
+
+const game = () => {
+    const playerScore 
+    for (let i = 0; i < 5; i++) {
