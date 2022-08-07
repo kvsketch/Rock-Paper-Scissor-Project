32fef6ce83a69084d9dd05f4f65c60b0389850b2
kvsketch <kellonv@gmail.com>
2022-08-07 11:56:14 -0600

    The game functions added animations to hover on buttons

--- javascript.js
+++ javascript.js
@@ -7,82 +7,92 @@ function getComputerChoice() {
 }
 
 const buttons = document.querySelectorAll("button");
-const input = [];
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    let result = input.push(`${button.id}`);
-    console.log(result);
-  });
-});
 
-function playerSelection() {
-  while (input.length > 0) {
-    if (input[0] == 'rock') {
-      return 'rock';
-    } else if (input[0] == 'paper') {
-      return 'paper';
-    } else if (input[0] == 'scissors') {
-      return 'scissors';
-    } else {
-      return console.log('ERROR');
-    }
-  }
-  input.splice(0,1);
+let startButton = document.querySelector("#start-game");
+startButton.addEventListener(
+  "click",
+  (start = () => {
+    startGame();
+    gameRound();
+  })
+);
+
+function startGame() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      playerSelection(`${button.id}`);
+    });
+  });
 }
 
-//adds to the player and computers score simplifying the game() loop function
+function playerSelection(el) {
+  playRound(el);
+}
 
-function playRound(player, computer) {
-  player = playerSelection();
+function playRound(player) {
   computer = getComputerChoice();
   console.log("player", player);
   console.log("computer", computer);
+
   if (
     (player === "rock" && computer === "scissors") ||
     (player === "paper" && computer === "rock") ||
     (player === "scissors" && computer === "paper")
   ) {
-    return "You win!", playerScore++;
-  } else if (player === computer) {
-    return "Tie!";
+    roundResult = "You win";
+    playerScore++;
+    round++;
+    updateScore();
+  } else if (player == computer) {
+    console.log("Tie");
   } else {
-    return "You lose!", computerScore++;
+    console.log("You lose");
+    computerScore++;
+    round++;
+    updateScore();
   }
+  gameRound();
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
-// const game = () => {
-//   for (i = 1; i < 6; i++) {
-//     playRound();
-//     console.log("round", i);
-//     console.log("playerscore", playerScore,
-//      "computerscore", computerScore);
-//     if (playerScore === 3 || computerScore === 3) {
-//       break;
-//     }
-//   }
-//   if (playerScore > computerScore) {
-//     console.log("You win");
-//     playAgain();
-//   } else if (computerScore > playerScore) {
-//     console.log("Computer man wins");
-//     playAgain();
-//   } else if (playerScore === computerScore) {
-//     console.log("Tie");
-//     playAgain();
-//   }
-// }
+const showPlayerScore = document.querySelector("#player_score");
+const showComputerScore = document.querySelector("#computer_score");
 
-// const playAgain = () => {
-//     let answer = prompt('Play again? Yes? No?');
-//     if (answer.toLowerCase() == 'yes') {
-//         i=1;
-//         playerScore = 0;
-//         computerScore =0;
-//         game();
-//     } else {
-//         console.log('Bye! Play again soon!')
-//     }
-// }
+function updateScore() {
+  showPlayerScore.textContent = `Your Score is ${playerScore}`;
+  showComputerScore.textContent = `Computer Score is ${computerScore}`;
+}
+const showRound = document.querySelector('.rounds');
+round = 1;
+function gameRound() {
+  showRound.textContent =  `Round ${round}`;
+  if (round < 5) {
+  if (playerScore >= 3 || playerScore > computerScore) {
+    console.log('You win');
+  } else if (computerScore >= 3 || playerScore < computerScore) {
+    console.log('Computer Wins');
+  } else if (playerScore == computerScore) {
+    console.log('No winner!')
+  }
+} else if (round >=5) {
+  console.log('Game Over'),
+  playAgain();
+}
+
+}
+
+
+const playAgain = () => {
+  const roundDiv = document.querySelector('.rounds');
+  const againBtn = document.createElement('button');
+  againBtn.textContent = "Play Again?";
+  roundDiv.appendChild(againBtn);
+  againBtn.addEventListener('click', play = () => {
+  round = 1;
+  playerScore = 0;
+  computerScore = 0;
+  updateScore();
+  });
+}
